45a515f26e75464729bd14131c6b1ae6b9166a0f
ljp <2junticket@gmail.com>
2022-08-25 23:09:00 +0900

    footer 수정완료

--- src/components/layout/js/footer.js
+++ src/components/layout/js/footer.js
@@ -1,35 +1,31 @@
 import React from "react";
-import Accordion from 'react-bootstrap/Accordion';
-//import Table from 'react-bootstrap/Table';
-//import logo from '../images/delunaLogo_128.png';
+import {Navbar, Container, Row, Col} from 'react-bootstrap';
 import '../css/footer.scss';
-
+import gitlogo from '../imges/github_logo.png';
 function Developer({ info }) {
     return (
-          <ul className="footer_ul">
-            <li><b>{info.position}</b></li>
-            {info.name_list.map(lists => (<Names namelist={lists} />)) }
-          </ul>
+          <Col sm>
+            <b className="title_text">{info.position}</b>
+            {info.name_list.map(lists => (<Names namelist={lists} />)) }   
+          </Col>
     );
  }
 
  function Names({namelist}){
     return (
-        <li>
-            {namelist.username}
-            <br/>
-            {namelist.email}
-        </li>
+        <p className="sub_text">
+            {namelist.username} <br/> {namelist.email}
+        </p>
     )
  }
 
  function SkillOut({skill_list}) {
-    const skillname = skill_list.skills.map((skill) => <li>{skill}</li>);
+    const skillname = skill_list.skills.map((skill) => <p className="skill_text">{skill}</p>);
     return (
-        <ul className="footer_ul">
-            <li>{skill_list.title} SKILLS</li>
-            {skillname}
-        </ul>
+        <Col sm>
+            <b className="title_text">{skill_list.title} SKILLS</b>
+            <p className="sub_text">{skillname}</p>
+        </Col>
     );
  }
 
@@ -37,7 +33,14 @@ function Developer({ info }) {
 const Footer = () => {
     let developer = [
         {
-            position : 'BACK-END',
+            position : 'PM',
+            name_list : [ {
+                username : '박문수',
+                email : 'cccccc@gmail.com'
+            }]
+        },
+        {
+            position : 'BACKEND DEVELOPER',
             name_list : [ {
                 username : '한동희',
                 email : 'aaaaaa@gmail.com'
@@ -48,7 +51,7 @@ const Footer = () => {
               }]
         },
         {
-            position : 'FRONT-END',
+            position : 'FRONTEND DEVELOPER',
             name_list : [{
                 username : '한예지',
                 email : 'tpdl5832@naver.com'
@@ -57,43 +60,41 @@ const Footer = () => {
                   username : '이준표',
                   email : '2junticket@gmail.com'
               }]
-        },
-        {
-            position : 'PM',
-            name_list : [ {
-                username : '박문수',
-                email : '@gmail.com'
-            }]
         }
     ];
     let git_addr = 'https://github.com/Hotel-Deluna';
     let skillarr = [
         {
-            title : 'back-end',
-            skills : ['java(openjdk17)', 'redis', 'spring boot', 'spring security', 'jwt', 'lombok']
+            title : 'BACKEND',
+            skills : ['java - open jdk 17', 'redis', 'spring boot', 'spring security', 'jwt', 'lombok']
         },
         {
-            title : 'front-end',
-            skills : ['node.js(16.17.0)', 'react(18.2.0)', 'scss', 'react-bootstrap', 'HTML5']
+            title : 'FRONTEND',
+            skills : ['node.js - 16.17.0', 'react - 18.2.0', 'react-bootstrap', 'scss', 'HTML5']
         },
         {
-            title : 'common',
-            skills : ['형상관리 : git', 'DB : mariaDB', 'cloud : aws, 카페24, oracle cloud', 'api 관리 : swagger']
+            title : 'COMMON',
+            skills : ['SERVER : aws','SCM : git', 'DB : mariaDB', 'CLOUD : aws, cafe24, oracle cloud', 'API Tools : swagger']
         }
     ];
     return (
-        <div id="footer" className="mt-auto">
-            <Accordion defaultActiveKey="0" >
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>
-                        {developer.map(list => (<Developer info={list} />)) }
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        {skillarr.map(skill_value => (<SkillOut skill_list={skill_value} />)) }
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-        </div>
+        <footer className="navbar-fixed-bottom">
+            <Navbar bg="gray" expand="lg">
+                <Container container-fluid>
+                    <Row className="vertical-center">
+                        {developer.map(list => (<Developer info={list} />))}
+                        <Col sm id="git_col">
+                            <Navbar.Brand href={git_addr} target="_blank">
+                                <img src={gitlogo} className="footer-logo" />
+                            </Navbar.Brand>
+                            <br />
+                            <b className="title_text">© 2022 Copyright: Hotel-Deluna</b>
+                        </Col>
+                        {skillarr.map(skill_value => (<SkillOut skill_list={skill_value} />))}
+                    </Row>
+                </Container>
+            </Navbar>
+        </footer>
     )
 };
 
