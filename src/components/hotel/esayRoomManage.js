502e32687d39639f672e2cdeb665a023c74fb625
hyj <tpdl5832@naver.com>
2022-09-08 13:18:25 +0900

    객실간편리스트 개발 90%진행 완료

--- src/components/hotel/esayRoomManage.js
+++ src/components/hotel/esayRoomManage.js
@@ -1,27 +1,59 @@
 import React, { useEffect, useState } from "react";
-import {Row, Col,Button} from 'react-bootstrap';
+import {Row, Col,Button, Table, InputGroup,Form} from 'react-bootstrap';
 import { UncontrolledCollapse, Card, CardBody } from 'reactstrap';
 import testImg from '../../pages/images/test.png';
 import * as hotelMainReducer from "../../modules/hotel/hotelMainReducer";
+import {my_hotel_list, hotel_code} from "../../modules/hotel/hotelMainActions";
+import { connect, useDispatch } from 'react-redux';
+
 import "./css/hotelRoomList.scss"
 import star from "./images/star.png";
 import noStar from "./images/no_star.png"
-/* redux 영역 */
-import { connect } from "react-redux";
-const EsayRoomManage = (props) => { 
-    const hotelList = props.form.list;
-    const codeList = props.code.code;
-    const [codeName, setcodeName] = useState([])
-    /*useEffect(() => {
-        for(var i=0; i<hotelList.length; i++){
-            for(var j=0; j<codeList.length; j++){
-                if(hotelList[i].tags.includes(codeList[j].code)){
-                    codeName.push(codeList.name)
-                }
+
+const EsayRoomManage = ({my_hotel_list,hotelList, hotel_code, hotelCode,form, code}) => {
+    const hotelListValue = form.list;
+    const codeList = code.code;
+    const [searchValue, setSearchValue] = useState('');
+    const [searchCont, setSearchCont] = useState(0);
+    const searchValueChange = (e) =>{
+        setSearchValue(e.target.value);
+    }
+    const search = () => {
+        setSearchCont(searchCont+1);
+    }
+    const dispatch = useDispatch();
+    
+    //진입 시 모든 리스트가 보여줘야 함
+    useEffect(() => {
+        my_hotel_list(searchValue);
+        hotel_code();
+    },[])
+
+    //사업자가 검색버튼 누를 때 리스트 재조회
+    useEffect(() => {
+        my_hotel_list(searchValue);
+    },[searchCont, setSearchCont]);
+
+    //호텔리스트 조회 상태에 따라 dispatch or 예외처리
+    useEffect(() => {
+        if(hotelList){
+            if(hotelList.result === 'OK'){
+                dispatch(hotelMainReducer.selectHotelList({ data : hotelList.data}));
+            }else{
+                alert("호텔 리스트 조회가 실패하였습니다. 잠시 후 다시 이용해주세요.");
             }
         }
-        setcodeName(codeName)
-    },[setcodeName])*/
+    },[my_hotel_list,hotelList])
+
+    //호텔 부가서비스/시설 공통코드 조회 상태에 따라 dispatch or 예외처리
+    useEffect(() => {
+        console.log(hotelCode)
+        if(hotelCode){
+            if(hotelCode.result === 'OK'){
+                dispatch(hotelMainReducer.selectHotelCode({ data : hotelCode.data}));
+            }
+        }
+    },[hotel_code,hotelCode])
 
     return (
         <>
@@ -30,7 +62,16 @@ const EsayRoomManage = (props) => {
                     객실 간편관리
                 </Col>
             </Row>
-            {hotelList.map((item, index) => (
+            <InputGroup id="hotelSearch" className="mb-3">
+            <Form.Control
+                placeholder="호텔명 입력"
+                onChange={searchValueChange}
+                />
+                <Button variant="outline-secondary" id="button-addon2" onClick={()=>search()}>
+                    Search
+                </Button>
+            </InputGroup>
+            {hotelListValue.map((item, index) => (
             <div className="card mb-3" key={index}>
                 <div className="row no-gutters">
                     <div className="col-md-4">
@@ -64,7 +105,7 @@ const EsayRoomManage = (props) => {
                                 </small>
                             
                             </p>
-                            <Button variant="outline-primary">
+                            <Button id="roomAdd" variant="outline-primary" onClick={()=>alert("객실 추가 & 등록 연동")}>
                                     {item.room_list.length > 0 ? '객실추가' : '객실등록'}
                             </Button>{' '}
                             {
@@ -76,8 +117,52 @@ const EsayRoomManage = (props) => {
                                     <UncontrolledCollapse toggler="#roomList" className="m-0 p-0">
                                         <Card>
                                             <CardBody>
-                                                리액트 나도 할 수 있다! 뚝딱~!
-                                            </CardBody>
+                                                <Table bordered id="esayRoomManageList">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>객실명</th>
+                                                            <th>객실수</th>
+                                                            <th>비성수기 가격</th>
+                                                            <th>성수기 가격</th>
+                                                            <th>상태</th>
+                                                            <th>수정/삭제</th>
+                                                            
+                                                        </tr>
+                                                    </thead>
+                                                        <tbody>
+                                                        {item.room_list.map((item2, index2) => (
+                                                            index2 < 3 ?
+                                                            <tr id="roomListTable" className="table-light" key={index2}>
+                                                                <td>{item2.name}</td>
+                                                                <td>{item2.reservable_room_count}</td>
+                                                                <td>
+                                                                    <p>평일 : {item2.weekday_price}</p>
+                                                                    <p>주말 : {item2.weekend_price}</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p>평일 : {item2.p_weekday_price}</p>
+                                                                    <p>주말 : {item2.p_weekend_price}</p>     
+                                                                </td>
+                                                                <td>
+                                                                    {
+                                                                        item2.available_yn ?
+                                                                        '예약가능' : 
+                                                                        
+                                                                        item2.room_closed_start +'~'+item2.room_closed_end+' 예약 불가능' 
+                                                                    }
+                                                                </td>
+                                                                <td id="roomCorrec">
+                                                                    <Button variant="outline-dark" onClick={() => alert("객실 수정 모달 연동")}>수정</Button>
+                                                                    <Button variant="outline-danger" onClick={() => alert("객실 삭제 모달 연동")}>삭제</Button>
+                                                                </td>
+                                                                
+                                                            </tr>
+                                                            :null
+                                                        ))}
+                                                        </tbody>
+                                                </Table>
+                                                <Button id="moreRoom" variant="outline-secondary" onClick={() => alert('객실상세 페이지 이동')}>객실 더보기</Button>
+                                            </CardBody> 
                                         </Card>
                                     </UncontrolledCollapse>
                                 </div>
@@ -96,8 +181,15 @@ const EsayRoomManage = (props) => {
     )
 };
 export default connect(
-    (state) => ({
-        form: state.hotelMainReducer.getIn(['MY_HOTEL_LIST', 'form']),
-        code : state.hotelMainReducer.getIn(['HOTEL_CODE', 'form'])
+    () =>  ({ hotelMainActions, hotelMainReducer}) => ({
+        hotelList: hotelMainActions.hotelList, //나(사업자)의 호텔리스트 조회 상태값
+        hotelCode : hotelMainActions.code,
+        form: hotelMainReducer.getIn(['MY_HOTEL_LIST', 'form']),
+        code : hotelMainReducer.getIn(['HOTEL_CODE', 'form'])
     }),
+    {
+        my_hotel_list, //나(사업자)의 호텔리스트 조회 액션
+        hotel_code
+
+    }
 )(EsayRoomManage);
--- src/pages/js/hotelRoomList.js
+++ src/pages/js/hotelRoomList.js
@@ -1,36 +1,11 @@
 /*global kakao*/
 import React, { useEffect } from "react";
 import {Container, Row, Col} from 'react-bootstrap';
-import {my_hotel_list, hotel_code} from "../../modules/hotel/hotelMainActions";
-import * as hotelMainReducer from '../../modules/hotel/hotelMainReducer';
-import { connect, useDispatch } from 'react-redux';
+
 import "../../components/hotel/css/hotelRoomList.scss"
 //객실 간편 관리
 import EsayRoomManage from "../../components/hotel/esayRoomManage";
-const HotelRoomList = ({my_hotel_list,hotelList, hotel_code, hotelCode}) => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        my_hotel_list("");
-        hotel_code();
-    },[])
-    useEffect(() => {
-        if(hotelList){
-            if(hotelList.result === 'OK'){
-                dispatch(hotelMainReducer.selectHotelList({ data : hotelList.data}));
-            }else{
-                alert("호텔 리스트 조회가 실패하였습니다. 잠시 후 다시 이용해주세요.");
-            }
-        }
-    },[my_hotel_list,hotelList])
-
-    useEffect(() => {
-        console.log(hotelCode)
-        if(hotelCode){
-            if(hotelCode.result === 'OK'){
-                dispatch(hotelMainReducer.selectHotelCode({ data : hotelCode.data}));
-            }
-        }
-    },[hotel_code,hotelCode])
+const HotelRoomList = () => {
     return (
         <>
         <Container className="roomListContainer">
@@ -40,14 +15,4 @@ const HotelRoomList = ({my_hotel_list,hotelList, hotel_code, hotelCode}) => {
     );
 };
 
-export default connect(
-    () =>  ({ hotelMainActions}) => ({
-        hotelList: hotelMainActions.hotelList, //나(사업자)의 호텔리스트 조회 상태값
-        hotelCode : hotelMainActions.code
-    }),
-    {
-        my_hotel_list, //나(사업자)의 호텔리스트 조회 액션
-        hotel_code
-
-    }
-)(HotelRoomList)
+export default HotelRoomList;
